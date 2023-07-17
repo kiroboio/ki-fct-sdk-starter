f20714fb657ad079a6779c540fa6d846cc394116
Diego <me@diego.earth>
2023-07-17 14:50:33 +0200

    add fcts actions

--- src/components/FCTCard.tsx
+++ src/components/FCTCard.tsx
@@ -1,7 +1,9 @@
-import { Badge, Box, ButtonGroup, Card, CardBody, HStack, IconButton, Stack, Text } from '@chakra-ui/react'
+import { Badge, Box, ButtonGroup, Card, CardBody, HStack, IconButton, Stack, Text, Spinner } from '@chakra-ui/react'
 import { useComputed, service } from '@kiroboio/fct-sdk'
 import { memo } from 'react'
 import { Icon } from '@iconify/react'
+
+import { pauseFCT, removeFCT, resumeFCT, signFCT } from '../utils/fcts'
 
 const FCTCard = ({ id }: { id: string }) => {
   const fcts = service.fct.active.data.fmt.map
@@ -9,6 +11,34 @@ const FCTCard = ({ id }: { id: string }) => {
   const status = useComputed(() => fcts.value[id]?.status)
   const gas_price_limit = useComputed(() => fcts.value[id]?.gas_price_limit)
   const createdAt = useComputed(() => fcts.value[id]?.createdAt)
+
+  const isRemoving = useComputed(() => service.fct.active.remove.isRunning(id).value)
+  const isSigning = useComputed(() => service.fct.active.addSignature.isRunning(id).value)
+  const isBlocked = useComputed(() => fcts.value[id]?.health.unblocked)
+
+  const signButton = useComputed(() =>
+    isSigning.value ? (
+      <Spinner marginLeft={3} marginRight={3} />
+    ) : (
+      <IconButton aria-label="Sign" icon={<Icon icon={`fluent:signature-28-filled`} />} colorScheme="whatsapp" onClick={() => signFCT(id)} />
+    )
+  )
+
+  const pauseButton = useComputed(() =>
+    isBlocked.value ? (
+      <IconButton aria-label="Pause" icon={<Icon icon={`fa6-solid:pause`} />} colorScheme="messenger" onClick={() => pauseFCT(id)} />
+    ) : (
+      <IconButton aria-label="Pause" icon={<Icon icon={`fa6-solid:pause`} />} colorScheme="messenger" onClick={() => resumeFCT(id)} />
+    )
+  )
+
+  const removeButton = useComputed(() =>
+    isRemoving.value ? (
+      <Spinner marginLeft={3} marginRight={3} />
+    ) : (
+      <IconButton aria-label="Delete" icon={<Icon icon={`ph:trash-bold`} />} colorScheme="red" onClick={() => removeFCT(id)} />
+    )
+  )
 
   return (
     <Card variant="outline">
@@ -28,9 +58,9 @@ const FCTCard = ({ id }: { id: string }) => {
               <Text>{gas_price_limit} gwai </Text>
             </HStack>
             <ButtonGroup size="xs" variant="outline" spacing={0.5}>
-              <IconButton aria-label="Sign" icon={<Icon icon={`fluent:signature-28-filled`} />} colorScheme="whatsapp" />
-              <IconButton aria-label="Pause" icon={<Icon icon={`fa6-solid:pause`} />} colorScheme="messenger" />
-              <IconButton aria-label="Delete" icon={<Icon icon={`ph:trash-bold`} />} colorScheme="red" />
+              <>{signButton}</>
+              <>{pauseButton}</>
+              <>{removeButton}</>
             </ButtonGroup>
           </HStack>
         </Stack>
