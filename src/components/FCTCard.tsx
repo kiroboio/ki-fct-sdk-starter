fbc40820e0141275e796d8f724ea2d14c16c69f7
Diego <me@diego.earth>
2023-07-17 14:24:27 +0200

    styling

--- src/components/FCTCard.tsx
+++ src/components/FCTCard.tsx
@@ -27,7 +27,7 @@ const FCTCard = ({ id }: { id: string }) => {
               <Icon icon={`tabler:gas-station`} width={18} />
               <Text as="strong">{gas_price_limit} Gwai </Text>
             </HStack>
-            <ButtonGroup size="xs" variant="outline" spacing={1}>
+            <ButtonGroup size="xs" variant="outline" colorScheme="teal" spacing={1} isAttached>
               <IconButton aria-label="Sign" icon={<Icon icon={`fluent:signature-28-filled`} />} />
               <IconButton aria-label="Pause" icon={<Icon icon={`fa6-solid:pause`} />} />
               <IconButton aria-label="Delete" icon={<Icon icon={`ph:trash-bold`} />} />
