f0645aee1c08838b9ba46220d1488b32a3a82ae7
Diego <me@diego.earth>
2023-07-17 14:50:33 +0200

    add fcts actions

---
+++ src/utils/fcts.ts
@@ -0,0 +1,38 @@
+import { service } from '@kiroboio/fct-sdk'
+
+const signFCT = async (id: string) => {
+  try {
+    await service.fct.active.addSignature.execute(id, {})
+  } catch (error) {
+    console.error('signFCT Error', error)
+  } finally {
+    console.log('signFCT state', service.fct.active.addSignature.state(id).value)
+  }
+}
+
+const pauseFCT = async (id: string) => {
+  try {
+    const res = await service.fct.active.block.execute(id, {})
+    console.log(res)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const resumeFCT = async (id: string) => {
+  try {
+    await service.fct.active.unblock.execute(id, {})
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const removeFCT = async (id: string) => {
+  try {
+    await service.fct.active.remove.execute(id, {})
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export { signFCT, pauseFCT, resumeFCT, removeFCT }
