3ea95a7e7c2fa05186040414fc39cb4656d7dc25
Diego <me@diego.earth>
2023-08-20 17:04:15 +0200

    transfer nft modal (wip)

--- src/components/TransferNFTModal.tsx
+++ src/components/TransferNFTModal.tsx
@@ -6,10 +6,20 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Button,
   Stack,
   HStack,
-  Text,
+  Image,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Checkbox,
+  Input,
+  Divider,
+  Button,
 } from '@chakra-ui/react'
 import { service, useComputed } from '@kiroboio/fct-sdk'
 import { useState } from 'react'
@@ -19,6 +29,18 @@ const TransferNFTModal = ({ isOpen, onClose, id, isWallet }: { isOpen: any; onCl
   const [error, setError] = useState('')
   const [isSending, setIsSending] = useState(false)
   const nfts = isWallet ? service.nfts.wallet.data.raw.map : service.nfts.vault.data.raw.map
+
+  const nft = useComputed(() => service.nfts[isWallet ? 'wallet' : 'vault'].data.fmt.map.value[id])
+  const amount = useComputed(() => nft.value?.amount)
+  const address = useComputed(() => nft.value?.token_address)
+  const name = useComputed(() => nft.value?.name)
+  const symbol = useComputed(() => nft.value?.symbol)
+  const tokenId = useComputed(() => nft.value?.token_id)
+  const logo = useComputed(() => (
+    <>
+      <Image width={150} height={150} src={nft.value?.iconUrl || ''} alt={nft.value?.name || ''} />
+    </>
+  ))
 
   const handleModalClose = () => {
     setTransferWalletAddress('')
@@ -32,34 +54,45 @@ const TransferNFTModal = ({ isOpen, onClose, id, isWallet }: { isOpen: any; onCl
       <Modal size="sm" isOpen={isOpen} onClose={handleModalClose} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>NFT Details</ModalHeader>
+          <ModalHeader textAlign="center">NFT Details</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Stack>
-              <HStack justify="space-between">
-                <Text>Contract Address</Text>
-                <Text></Text>
-              </HStack>
-              <HStack justify="space-between">
-                <Text>Token Id</Text>
-                <Text>${id}</Text>
-              </HStack>
-              <HStack justify="space-between">
-                <Text>Token Standard</Text>
-                <Text></Text>
-              </HStack>
-              <HStack justify="space-between">
-                <Text>Blockchain</Text>
-                <Text></Text>
-              </HStack>
+            <Stack spacing={6}>
+              <HStack justifyContent="center">{logo}</HStack>
+              <TableContainer>
+                <Table variant="simple">
+                  <Thead>
+                    <Tr>
+                      <Th>ID</Th>
+                      <Th>Name</Th>
+                      <Th>Amount</Th>
+                      <Th>Symbol</Th>
+                      <Th>Address</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td>{tokenId}</Td>
+                      <Td>{name}</Td>
+                      <Td>{amount}</Td>
+                      <Td>{symbol}</Td>
+                      <Td>{address}</Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
+              <Divider />
+              <Stack spacing={2}>
+                <Checkbox>Move to your {isWallet ? 'smart wallet' : 'connected wallet'}</Checkbox>
+                <Input disabled={false} placeholder="Ethereum Address" value="" />
+              </Stack>
             </Stack>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
+            <Button w="full" colorScheme="blue">
+              Send
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
